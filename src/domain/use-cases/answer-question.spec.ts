f5c6873caf9c4880e0e5b9d39a9e1e2b78d2d1a5
Guilherme <gui.barros.js@gmail.com>
2023-08-25 00:40:25 -0300

    Fundamentos de DDD - Dependências externas

--- src/domain/use-cases/answer-question.spec.ts
+++ src/domain/use-cases/answer-question.spec.ts
@@ -1,10 +1,16 @@
 import { expect, test } from 'vitest'
 import { AnswerQuestionUseCase } from './answer-question'
+import { Answer } from '../entities/answer'
+import { AnswersRepository } from '../repositories/answers-repository'
 
-test('Create an answer', () => {
-  const answerQuestion = new AnswerQuestionUseCase()
+const fakeAnswersRepository: AnswersRepository = {
+  create: async (answer: Answer) => {}
+}
 
-  const { answer } = answerQuestion.execute({
+test('Create an answer', async () => {
+  const answerQuestion = new AnswerQuestionUseCase(fakeAnswersRepository)
+
+  const { answer } = await answerQuestion.execute({
     instructorId: '1',
     questionId: '1',
     content: 'Nova resposta',
