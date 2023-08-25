f0e530a702237e08f45c2c13859898267a1d3049
Guilherme <gui.barros.js@gmail.com>
2023-08-25 17:49:33 -0300

    Fundamentos de DDD - Getters & Setters das entidades

--- src/domain/entities/answer.ts
+++ src/domain/entities/answer.ts
@@ -11,8 +11,38 @@ interface AnswerProps {
 }
 
 export class Answer extends Entity<AnswerProps> {
+  get authorId() {
+    return this.props.authorId
+  }
+
+  get questionId() {
+    return this.props.questionId
+  }
+
   get content() {
     return this.props.content
+  }
+
+  get createdAt() {
+    return this.props.createdAt
+  }
+
+  get updatedAt() {
+    return this.props.updatedAt
+  }
+
+  get excerpt() {
+    return this.content.substring(0, 120).trimEnd().concat('...')
+  }
+
+  set content(content: string) {
+    this.props.content = content
+
+    this.touch()
+  }
+
+  private touch() {
+    this.props.updatedAt = new Date()
   }
 
   static create(
@@ -21,7 +51,7 @@ export class Answer extends Entity<AnswerProps> {
   ) {
     const answer = new Answer({
       ...props,
-      createdAt: new Date()
+      createdAt: props.createdAt ?? new Date()
     }, id)
 
     return answer
--- src/domain/entities/question.ts
+++ src/domain/entities/question.ts
@@ -1,3 +1,4 @@
+import dayjs from 'dayjs'
 import { Slug } from './value-objects/slug'
 import { Entity } from '../../core/entities/entity'
 import { UniqueEntityID } from '../../core/entities/unique-entity-id'
@@ -14,13 +15,73 @@ interface QuestionProps {
 }
 
 export class Question extends Entity<QuestionProps> {
+  get authorId() {
+    return this.props.authorId
+  }
+
+  get bestAnswerId() {
+    return this.props.bestAnswerId
+  }
+  
+  get title() {
+    return this.props.title
+  }
+  
+  get slug() {
+    return this.props.slug
+  }
+
+  get content() {
+    return this.props.content
+  }
+
+  get createdAt() {
+    return this.props.createdAt
+  }
+
+  get updatedAt() {
+    return this.props.updatedAt
+  }
+
+  get isNew() {
+    return dayjs().diff(this.createdAt, 'days') <= 3
+  }
+
+  get excerpt() {
+    return this.content.substring(0, 120).trimEnd().concat('...')
+  }
+
+  set title(title: string) {
+    this.props.title = title
+
+    this.props.slug = Slug.generateFromText(title)
+    this.touch()
+  }
+
+  set content(content: string) {
+    this.props.content = content
+
+    this.touch()
+  }
+
+  set bestAnswerId(bestAnswerId: UniqueEntityID | undefined) {
+    this.props.bestAnswerId = bestAnswerId
+
+    this.touch()
+  }
+
+  private touch() {
+    this.props.updatedAt = new Date()
+  }
+
   static create(
-    props: Optional<QuestionProps, 'createdAt'>,
+    props: Optional<QuestionProps, 'createdAt' | 'slug'>,
     id?: UniqueEntityID
   ) {
     const question = new Question({
       ...props,
-      createdAt: new Date()
+      slug: props.slug ?? Slug.generateFromText(props.title),
+      createdAt: props.createdAt ?? new Date()
     }, id)
 
     return question
