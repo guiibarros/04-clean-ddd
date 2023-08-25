8b39b4dd0a916bc47d1dc6996243146b42954200
Guilherme <gui.barros.js@gmail.com>
2023-08-25 01:41:03 -0300

    Fundamentos de DDD - Value Object de slug

---
+++ src/domain/entities/value-objects/slug.spec.ts
@@ -0,0 +1,8 @@
+import { expect, test } from 'vitest'
+import { Slug } from './slug'
+
+test('it should be able to generate a new slug from text', () => {
+  const slug = Slug.generateFromText('Example question title')
+
+  expect(slug.value).toEqual('example-question-title')
+})
