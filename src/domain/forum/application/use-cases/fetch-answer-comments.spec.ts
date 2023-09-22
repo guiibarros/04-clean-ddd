0410ad532a708efee75f63ec4f9119ea7d63fafc
Guilherme <gui.barros.js@gmail.com>
2023-09-22 00:27:20 -0300

    Fórum com DDD & Clean Architecture - Caso de uso: Listar comentários da resposta

---
+++ src/domain/forum/application/use-cases/fetch-answer-comments.spec.ts
@@ -0,0 +1,60 @@
+import { AnswerCommentFactory } from 'test/factories/answer-comment-factory'
+import { InMemoryAnswerCommentsRepository } from 'test/repositories/in-memory-answer-comments-repository'
+
+import { UniqueEntityID } from '@/core/entities/unique-entity-id'
+
+import { FetchAnswerCommentsUseCase } from './fetch-answer-comments'
+
+let answerCommentsRepository: InMemoryAnswerCommentsRepository
+let sut: FetchAnswerCommentsUseCase
+
+describe('Fetch answer comments', () => {
+  beforeEach(() => {
+    answerCommentsRepository = new InMemoryAnswerCommentsRepository()
+    sut = new FetchAnswerCommentsUseCase(answerCommentsRepository)
+  })
+
+  it('should be able to fetch answer comments', async () => {
+    await answerCommentsRepository.create(
+      AnswerCommentFactory.make({
+        answerId: new UniqueEntityID('answer-1'),
+      }),
+    )
+
+    await answerCommentsRepository.create(
+      AnswerCommentFactory.make({
+        answerId: new UniqueEntityID('answer-1'),
+      }),
+    )
+
+    await answerCommentsRepository.create(
+      AnswerCommentFactory.make({
+        answerId: new UniqueEntityID('answer-1'),
+      }),
+    )
+
+    const { answerComments } = await sut.execute({
+      answerId: 'answer-1',
+      page: 1,
+    })
+
+    expect(answerComments).toHaveLength(3)
+  })
+
+  it('should be able to fetch paginated answer comments', async () => {
+    for (let i = 1; i <= 22; i++) {
+      await answerCommentsRepository.create(
+        AnswerCommentFactory.make({
+          answerId: new UniqueEntityID('answer-1'),
+        }),
+      )
+    }
+
+    const { answerComments } = await sut.execute({
+      answerId: 'answer-1',
+      page: 2,
+    })
+
+    expect(answerComments).toHaveLength(2)
+  })
+})
